73ed7837028fcfb6f9d39d88b75912eb15b59527
OLIVER BADGER <c0022288@hallam.shu.ac.uk>
2021-05-01 22:02:36 +0100

    Done the the Id for Teacher

--- src/BUE/Main.java
+++ src/BUE/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("LoginStudent.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginTeacher.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 660, 469));
         primaryStage.show();
